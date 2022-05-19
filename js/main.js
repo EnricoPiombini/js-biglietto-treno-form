6a70eebcc5387e82c099df87c2e97138539ab176
EnricoPiombini <piombini.enrico@gmail.com>
2022-05-19 15:59:10 +0200

    Funzione add listener

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 const inputName = document.querySelector("[name='userName']");
 const inputKm = document.querySelector ("[name='numKm']");
 const inputAge = document.querySelector ("[name='age']");
+const submitButton = document.getElementById('submitButton')
+
+submitButton.addEventListener("click", function () {
+console.log(inputName.value, inputKm.value);
+});
